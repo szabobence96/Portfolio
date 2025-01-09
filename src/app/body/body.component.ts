7dc36ef6ccdcd115bd7358a2754ec7b5f88e3cd8
szabobence96 <sz.bence996@gmail.com>
2025-01-09 22:03:09 +0100

    fix bg image

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,26 +1,42 @@
 import { animate, state, style, transition, trigger } from '@angular/animations';
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
   styleUrls: ['./body.component.scss']
 })
-export class BodyComponent implements OnInit{
+export class BodyComponent implements OnInit {
+  screenwidth = 0;
 
-  bgUrl:string="./assets/images/bg_2.jpg";
-  bgUrl2:string="https://image.ibb.co/n5A2HU/showcase.jpg";
-
+  currentImg: any;
+  bgPc: string = "./assets/images/showcase.jpg";
+  bgMobile: string = "./assets/images/bg_mobile2.jpg";
   private imageTimeout: any;
- 
+
   ngOnInit() {
-   
+    this.screenwidth = window.innerWidth;
     this.imageTimeout = setTimeout(() => {
       const imageCity = document.querySelector('.img-city') as HTMLElement;
       if (imageCity) {
         imageCity.classList.add('fade-in');
       }
     }, 200);
+    this.setCurrentImage();  // Képméret frissítése ablakméret változáskor
 
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.screenwidth = window.innerWidth;
+    this.setCurrentImage();  // Képméret frissítése ablakméret változáskor
+
+  }
+  setCurrentImage() {
+    if (this.screenwidth < 768) {
+      this.currentImg = this.bgMobile;  // Mobil nézet
+    } else {
+      this.currentImg = this.bgPc;      // Asztali nézet
+    }
   }
 }
--- src/app/sections/second/second.component.ts
+++ src/app/sections/second/second.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { TextSecondService } from './text-second.service';
 
 @Component({
@@ -8,9 +8,16 @@ import { TextSecondService } from './text-second.service';
 })
 export class SecondComponent {
 
-  constructor(public textservice: TextSecondService) { }
+  constructor(public textservice: TextSecondService) {
+    this.updateImagesBasedOnScreenWidth();
+  }
   showOverlay: boolean = true;
+  screenWidth: number = 0;
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.updateImagesBasedOnScreenWidth();
+  }
   images = [
     {
       imageSrc: './assets/images/webshop_images/w2.png',
@@ -30,8 +37,32 @@ export class SecondComponent {
     {
       imageSrc: './assets/images/webshop_images/w7.png',
     },
+  ]
+
+  mediumImages = [
+    {
+      imageSrc: './assets/images/webshop_images/w_m1.png',
+    },
+    {
+      imageSrc: './assets/images/webshop_images/w_m2.png',
+    },
+    {
+      imageSrc: './assets/images/webshop_images/w_m3.png',
+    },
+    {
+      imageSrc: './assets/images/webshop_images/w_m4.png',
+    },
+    {
+      imageSrc: './assets/images/webshop_images/w_m5.png',
+    },
+    {
+      imageSrc: './assets/images/webshop_images/w_m6.png',
+    },
 
   ]
   currentImages: any[] = this.images;
-
+  private updateImagesBasedOnScreenWidth() {
+    this.screenWidth = window.innerWidth;
+    this.currentImages = this.screenWidth < 768 ? this.mediumImages : this.images;
+  }
 }
